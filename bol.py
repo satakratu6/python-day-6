678e4928933b1ab0af9d60bcfd12b8faa06c6a8e
satakratu6 <115583018+satakratu6@users.noreply.github.com>
2022-10-15 13:31:02 +0530

    Add files via upload

---
+++ bol.py
@@ -0,0 +1,104 @@
+# Booleans
+# true of false bool
+# print(20 > 11)
+# print(20 = 11)
+# print(20 < 11)
+# print(bool('MY NAME IS'))
+# print(bool(30))
+#Lists
+
+ # mylist= [23,34,56,78,90]
+# print(mylist)
+
+# Python Operators
+# Arithmetic Operators
+# print(10+20) #addition
+# print(20-10) #subtraction
+# print(20*10) #multiplication
+# print(20/10) #division
+# print(20%10) #modulus
+# print(15// 2) #floor division 7.5 but its rounds the result to the nearest whole number
+# print(15// 4)
+# print(15/4)
+
+# Assignment Operators
+# a= 20
+# a +=5
+# print(a)
+#a -=5
+# print(a)
+# a *= 5
+# print(a)
+# a /= 5
+# print(a)
+# print (3 ** 2) #exponential 
+# a %= 5
+# print(a)
+# a //=3
+# print(a)
+# a **=2
+# print(a)
+
+
+# Comparison
+# print(3 == 4) #Equal
+# print(3 !=4) #Not Equal
+# print(3>4) #greater than
+# print(3<4) #less than
+# print(3>=4) #greater than equal
+# print(3<=4) #less than equal
+# logical operators
+x = 2
+# print(x<5 and x<8)
+# print(x < 5 and x>8)
+# print(x < 5 or x > 8) #Return True, if anyone of the condition is ture
+# print(x>3 or x >10)
+# print(not(x < 5 and x <8))
+
+#identiy Operators
+a = [10,20]
+b = [10,20]
+c=a
+# a.remove(20)
+# print(a)
+# print(b)
+# print(c)
+print(b is c)
+
+
+# print(a is c) #Returns true if both the vairables are the same object pointing to the same memory location
+# print(b is c) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
